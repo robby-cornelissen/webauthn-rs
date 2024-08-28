2187ca38932e0ffb3c8724d0b39b0eb1aa844363
robby-cornelissen <robby.cornelissen@gmail.com>
2024-08-28 10:07:38 +0900

    Remove old WIP

--- webauthn-authenticator-rs/src/lib.rs
+++ webauthn-authenticator-rs/src/lib.rs
@@ -133,10 +133,6 @@ pub mod error;
 #[cfg(any(all(doc, not(doctest)), feature = "vendor-yubikey"))]
 mod tlv;
 #[cfg(any(all(doc, not(doctest)), feature = "ctap2"))]
-pub mod manager;
-#[cfg(any(all(doc, not(doctest)), feature = "ctap2"))]
-pub mod manager;
-#[cfg(any(all(doc, not(doctest)), feature = "ctap2"))]
 pub mod transport;
 pub mod types;
 pub mod ui;
